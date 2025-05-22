758db6def54a61de1002ae04e4644af86fc3b87c
Por Cho <porla113@gmail.com>
2025-05-22 14:25:30 +0700

    create get_todos_csv and write_todos_csv funtions, add todos.csv file

--- functions.py
+++ functions.py
@@ -1,3 +1,42 @@
+import csv
+
+# get_todos_csv function
+def get_todos_csv(csv_filepath):
+    """
+    Read a csv file and return a list of to-do dict (to-do, date, time)
+    Arguments:
+    - csv_filepath - string
+    Return:
+    - a list of to-do dict
+    """
+    todos = []
+    with open(csv_filepath) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            todos.append(row)
+    return todos
+
+# write_todos_csv function
+def write_todos_csv(csv_filepath, todos_list):
+    """
+    Write a list of to-do dict (to-do, date, time) to a csv file
+    Arguments:
+    - csv_filepath: string
+    - todos_list: a list of to-do dict
+    Return:
+    - None
+    """
+    with open(csv_filepath, 'w', newline='') as csv_file:
+        fieldnames = ['to-do', 'date', 'time']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        csv_writer.writeheader()
+        for row in todos_list:
+            csv_writer.writerow(row)
+
+    return  None
+
 # get_todos function
 def get_todos(filepath):
     """
